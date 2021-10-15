a0ffd00d404a091b8b568996866580f590774b6c
TanuLamba3 <lambatanu3@gmail.com>
2021-10-15 03:49:39 -0700

    news fragments

--- app/src/main/java/com/example/narsinghjan/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/narsinghjan/ui/home/HomeFragment.java
@@ -14,6 +14,7 @@ import com.example.narsinghjan.LinkActivity;
 import com.example.narsinghjan.R;
 import com.example.narsinghjan.databinding.FragmentAboutusBinding;
 import com.example.narsinghjan.databinding.FragmentHomeBinding;
+import com.example.narsinghjan.ui.aboutus.AboutUsFragment;
 
 
 public class HomeFragment extends Fragment {
@@ -24,9 +25,22 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        TextView moreTxt = view.findViewById(R.id.moreTxt);
+
+        moreTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("key",moreTxt.getText().toString());
+                AboutUsFragment aboutUsFragment = new AboutUsFragment();
+                aboutUsFragment.setArguments(bundle);
+                getFragmentManager().beginTransaction().replace(R.id.moreTxt,aboutUsFragment).commit();
+            }
+        });
 
         // Inflate the layout for this fragment
-        return root;
+        return view;
     }
     @Override
     public void onDestroyView() {
